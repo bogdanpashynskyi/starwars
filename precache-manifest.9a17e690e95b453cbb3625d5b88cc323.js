ebefa62e92a1ce195f89fe0efc47c864a68c89e5
Bogdan Pashynskyi <bogdanpashynskyi@Bogdans-MacBook-Pro.local>
2019-03-05 18:46:57 +0200

    Updates

---
+++ precache-manifest.9a17e690e95b453cbb3625d5b88cc323.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1fdd3eb65d6d889808d",
+    "url": "/starwars/static/css/main.0c981613.chunk.css"
+  },
+  {
+    "revision": "f1fdd3eb65d6d889808d",
+    "url": "/starwars/static/js/main.f1fdd3eb.chunk.js"
+  },
+  {
+    "revision": "95e65c6947d44018319e",
+    "url": "/starwars/static/js/runtime~main.95e65c69.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/starwars/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/starwars/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5692c90812648627b29a2e2b0eea8a40",
+    "url": "/starwars/index.html"
+  }
+];
